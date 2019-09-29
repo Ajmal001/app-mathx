7116469314708f35deb85ebb7319e5b65a4cc616
mmahapat <mmahapat@asu.edu>
2019-09-28 17:28:01 -0700

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
-public class Controller {
+import com.firebase.client.Firebase;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private AnchorPane pane1;
+
+    @FXML
+    private AnchorPane pane2;
+
+    @FXML
+    private AnchorPane pane3;
+
+    @FXML
+    private AnchorPane pane4;
+
+    @FXML
+    void registerAction(ActionEvent actionEvent)
+    {
+        Firebase firebase=new Firebase("https://mathx-eea50.firebaseio.com/");
+        firebase.child("teacher_name").setValue("Manas");
+
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
 }
+
