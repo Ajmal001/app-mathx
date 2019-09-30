19d776529e960a056f7ef5114fb9e4500afd4751
mmahapat <mmahapat@asu.edu>
2019-09-30 15:38:07 -0700

    models/TeacherSignUpModel.java

---
+++ src/main/java/models/TeacherSignUpModel.java
@@ -0,0 +1,85 @@
+package mathx.models;
+
+public class TeacherSignUpModel {
+
+    String name,photo,summary,userName,password,address,phoneNo,speciality;
+
+    TeacherSignUpModel(){
+
+    }
+
+    public TeacherSignUpModel(String name, String photo, String summary, String userName, String password, String address, String phoneNo, String speciality) {
+        this.name = name;
+        this.photo = photo;
+        this.summary = summary;
+        this.userName = userName;
+        this.password = password;
+        this.address = address;
+        this.phoneNo = phoneNo;
+        this.speciality = speciality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
