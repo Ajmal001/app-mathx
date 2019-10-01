b69db463cc5531b0bf23d90aa2e52112d1804e7c
Sheran-12 <sdomini1@asu.edu>
2019-09-30 21:46:59 -0700

    Second commit

--- Authorization/src/sample/Main.java
+++ Authorization/src/sample/Main.java
@@ -10,8 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Login Page");
+        Parent root = FXMLLoader.load(getClass().getResource("Adminpage.fxml"));
+
+        primaryStage.setTitle("Admin Page");
         primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
