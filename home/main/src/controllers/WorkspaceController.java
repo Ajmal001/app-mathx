8ebeb6b46e019e8170da290a6e3de3ccfd742a5d
dracarys <kgrewal2@asu.edu>
2019-10-04 14:24:41 -0700

    -

--- home/main/src/controllers/WorkspaceController.java
+++ home/main/src/controllers/WorkspaceController.java
@@ -1,13 +1,16 @@
 package main.src.controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -17,11 +20,21 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import javax.lang.model.type.NullType;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+public class WorkspaceController implements Initializable {
 
-public class workspaceController implements Initializable {
+    @FXML
+    private VBox operatorSection;
+
+    @FXML
+    private Button addOperator;
+
+    @FXML
+    private Pane practiceSection;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
