ae4f25e2dd54de04c73940c0218cc52a4c25dffa
dracarys <kgrewal2@asu.edu>
2019-10-04 17:02:13 -0700

    Sprint 1 Complete

--- home/main/src/controllers/SignUpController.java
+++ home/main/src/controllers/SignUpController.java
@@ -1,5 +1,5 @@
 package main.src.controllers;
-import com.firebase.client.Firebase;
+//import com.firebase.client.Firebase;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -42,7 +42,7 @@ public class SignUpController implements Initializable {
     void registerAction(ActionEvent actionEvent)
     {
         System.out.println("called signup");
-        Firebase firebase=new Firebase("https://mathx-eea50.firebaseio.com/");
+//        Firebase firebase=new Firebase("https://mathx-eea50.firebaseio.com/");
         //firebase.child("Manas").setValue("Mahapatra");
         TeacherSignUpModel model=new TeacherSignUpModel();
 
@@ -57,7 +57,7 @@ public class SignUpController implements Initializable {
 //  //      model.setPhoneNumber(phoneNumberTF.getText());
 //
 
-        firebase.child("teachers").push().setValue(model);
+ //       firebase.child("teachers").push().setValue(model);
         new MainClass().openLoginWindow();
         MainClass.signUpStage.close();
         //goto login
--- home/main/src/controllers/WorkspaceController.java
+++ home/main/src/controllers/WorkspaceController.java
@@ -4,18 +4,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,83 +21,131 @@ import java.util.ResourceBundle;
 
 public class WorkspaceController implements Initializable {
 
-    private StackPane newRectangle(){
+    @FXML
+    private Pane practiceSection;
+    @FXML
+    private VBox operatorSection;
+    @FXML
+    private Label resetButton;
 
-        Rectangle shape=new Rectangle(190,70);
+    private StackPane newRectangle() {
+
+        Rectangle shape = new Rectangle(190, 70);
         shape.setArcWidth(30.0);
         shape.setArcHeight(30.0);
         shape.setStroke(Color.WHITE);
-        shape.setFill(Color.color(Math.random(),Math.random(),Math.random()));
+        shape.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+
 
         MenuItem menuItem1a = new MenuItem("1");
-        MenuItem menuItem2a= new MenuItem("2");
+        MenuItem menuItem2a = new MenuItem("2");
         MenuItem menuItem3a = new MenuItem("3");
-        MenuButton numberOne=new MenuButton("1",null,menuItem1a,menuItem2a,menuItem3a);
+        MenuItem menuItem4a = new MenuItem("4");
+        MenuItem menuItem5a = new MenuItem("5");
+        MenuItem menuItem6a = new MenuItem("6");
+        MenuItem menuItem7a = new MenuItem("7");
+        MenuItem menuItem8a = new MenuItem("8");
+        MenuItem menuItem9a = new MenuItem("9");
+
+        MenuButton numberOne = new MenuButton("1", null, menuItem1a, menuItem2a, menuItem3a,menuItem4a,menuItem5a,menuItem6a ,menuItem7a,menuItem8a,menuItem9a);
 
         MenuItem menuItem1b = new MenuItem("+");
         MenuItem menuItem2b = new MenuItem("-");
         MenuItem menuItem3b = new MenuItem("/");
-        MenuButton operator=new MenuButton("+",null,menuItem1b,menuItem2b,menuItem3b);
+        MenuButton operator = new MenuButton("+", null, menuItem1b, menuItem2b, menuItem3b);
 
         MenuItem menuItem1c = new MenuItem("1");
         MenuItem menuItem2c = new MenuItem("2");
         MenuItem menuItem3c = new MenuItem("3");
-        MenuButton numberTwo=new MenuButton("1",null,menuItem1c,menuItem2c,menuItem3c);
+
+        MenuButton numberTwo = new MenuButton("1", null, menuItem1c, menuItem2c, menuItem3c);
 
 
-        HBox shapeHBox=new HBox(numberOne,operator,numberTwo);
+        HBox shapeHBox = new HBox(numberOne, operator, numberTwo);
         shapeHBox.setAlignment(Pos.CENTER);
         shapeHBox.setSpacing(10);
 
-//        StackPane shapeStack=new StackPane(shape,shapeHBox);
-
-
-        return new StackPane(shape,shapeHBox);
+        return new StackPane(shape, shapeHBox);
     }
 
-    @FXML
-    private Pane practiceSection;
 
     public void setPracticeSection(Pane practiceSection) {
         this.practiceSection = practiceSection;
-        practiceSection.setPadding(new Insets(20,20,20,20));
+        practiceSection.setPadding(new Insets(20, 20, 20, 20));
+
+        StackPane sampleDrag = newRectangle();
+        practiceSection.getChildren().add(sampleDrag);
+
+
     }
 
-    @FXML
-    private VBox operatorSection;
-    private Text source = new Text(50, 100, "DRAG ME");
-    private Text target = new Text(300, 100, "DROP HERE");
-
-    int a=0,j=0,i=0;
+    int a = 0, j = 0, i = 0;
 
     private void setOperatorSection(VBox operatorSection) {
         this.operatorSection = operatorSection;
         operatorSection.setAlignment(Pos.BASELINE_CENTER);
-        StackPane equation1=newRectangle();
+        StackPane equation1 = newRectangle();
         operatorSection.getChildren().add(equation1);
-        //equation1.setOnMouseClicked(mouseEvent -> {
-//            practiceSection.getChildren().add(newRectangle());
-//            practiceSection.getChildren().get(a).relocate(i,j);
-//            i+=190;
-//            if(i>4*190){
-//                i=0;
-//                j+=90;
-//            }
-//            a++;
-//        });
-        source.setOnDragDetected(event -> {
-            Dragboard db = source.startDragAndDrop(TransferMode.ANY);
-            ClipboardContent content = new ClipboardContent();
-            content.putString(source.getText());
-            db.setContent(content);
+        equation1.setOnMouseClicked(mouseEvent -> {
+            practiceSection.getChildren().add(newRectangle());
+            practiceSection.getChildren().get(a).relocate(i, j);
+            i += 190;
+            if (i > 4 * 190) {
+                i = 0;
+                j += 70;
+            }
+            a++;
+        });
+        final double[] deltaX = new double[1];
+        final double[] deltaY = new double[1];
 
-            event.consume();
+
+        try {
+
+            deltaX[0]=0;
+            deltaY[0]=0;
+            practiceSection.getChildren().get(0).setOnMouseClicked(mouseEvent -> {
+                deltaX[0] = practiceSection.getChildren().get(0).getLayoutX() - mouseEvent.getSceneX();
+                deltaY[0] = practiceSection.getChildren().get(0).getLayoutY() - mouseEvent.getSceneY();
+                System.out.println(deltaX[0]);System.out.println(deltaY[0]);
+
+            });
+
+            practiceSection.getChildren().get(0).setOnMouseReleased(mouseEvent -> {
+                practiceSection.getChildren().get(0).setCursor(Cursor.HAND);
+            });
+
+
+            practiceSection.getChildren().get(0).setOnMouseEntered(mouseEvent -> {
+                practiceSection.getChildren().get(0).setCursor(Cursor.MOVE);
+            });
+
+            practiceSection.getChildren().get(0).setOnMouseDragged(mouseEvent -> {
+                practiceSection.getChildren().get(0).setLayoutX(mouseEvent.getSceneX() + deltaX[0]);
+                practiceSection.getChildren().get(0).setLayoutY(mouseEvent.getSceneY() + deltaY[0]);
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setResetButton(Label resetButton) {
+        this.resetButton = resetButton;
+        resetButton.setOnMouseClicked(mouseEvent -> {
+            practiceSection.getChildren().clear();
+            i=0;j=0;a=0;
         });
     }
 
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        practiceSection.getChildren().addAll(source,target);
+        setPracticeSection(practiceSection);
         setOperatorSection(operatorSection);
+        setResetButton(resetButton);
     }
+
+
+
 }
