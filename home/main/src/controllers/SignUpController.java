cfb95a977604eab51d8bfbc01f28e2e04c89d202
adityab079 <36786132+adityab079@users.noreply.github.com>
2019-10-10 20:56:30 -0700

    Checking for a valid Password during Sign up; tweaking Error message

--- home/main/src/controllers/SignUpController.java
+++ home/main/src/controllers/SignUpController.java
@@ -33,8 +33,8 @@ public class SignUpController implements Initializable {
 
     private void showAlert(String message){
 
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Warning");
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Signup Form Error");
      //   alert.setHeaderText("Required Fields Empty");
         alert.setContentText(message);
         alert.showAndWait();
@@ -51,11 +51,15 @@ public class SignUpController implements Initializable {
 //Checking if Name is not empty
         if (name.isEmpty()){
             showAlert("Please enter your name");
+            nameTF.requestFocus();
+            return;
         }
 
 //Checking if Email is not empty
         else if (email.isEmpty()){
             showAlert("Please enter your email id ");
+            emailTF.requestFocus();
+            return;
         }
 
 //Checking if Email is valid
@@ -64,17 +68,54 @@ public class SignUpController implements Initializable {
         Matcher m =p.matcher(email);
         if(!m.matches()) {
             showAlert("Please enter a valid email id");
+            emailTF.requestFocus();
+            return;
         }
 
+//Checking if Password is not empty
         else if (pswd.isEmpty()){
-            showAlert("Please enter your email id ");
+            showAlert("Please enter a Password ");
+            pswdTF.requestFocus();
+            return;
         }
-        else if (rpswd.isEmpty()){
-            showAlert("please confirm password ");
+
+//Checking if Password is valid
+        Pattern specialCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Pattern UpperCasePatten = Pattern.compile("[A-Z ]");
+        Pattern lowerCasePatten = Pattern.compile("[a-z ]");
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+
+        String error="";
+
+        if (pswd.length() < 8) {
+            error+="-> Password must have atleast 8 characters\n";
         }
-        else if (!pswdTF.getText().equals(rpswdTF.getText())){
-            showAlert("passwords do not match ");
+        if (pswd.length() > 20) {
+            error+="-> Password must have atmost 20 characters\n";
         }
+        if (!specialCharPatten.matcher(pswd).find()) {
+            error+="-> Password must have atleast one special character\n";
+        }
+        if (!UpperCasePatten.matcher(pswd).find()) {
+            error+="-> Password must have atleast one uppercase character\n";
+        }
+        if (!lowerCasePatten.matcher(pswd).find()) {
+            error+="-> Password must have atleast one lowercase character\n";
+        }
+        if (!digitCasePatten.matcher(pswd).find()) {
+            error+="-> Password must have atleast one digit character\n";
+        }
+        if(error.length()>0) {
+            showAlert("Please re-enter your password\n"+error);
+            pswdTF.requestFocus();
+        }
+
+        else if (!pswd.equals(rpswd)) {
+            showAlert("Passwords do not match ");
+            rpswdTF.requestFocus();
+            return;
+        }
+
         else
         {
             //push data to firebase
