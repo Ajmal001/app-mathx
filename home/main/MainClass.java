1df629e198a6a7ab87e67876041f9de0453a9b82
adityab079 <36786132+adityab079@users.noreply.github.com>
2019-11-09 12:22:34 -0700

    Changed primary stage

--- home/main/MainClass.java
+++ home/main/MainClass.java
@@ -22,7 +22,7 @@ public class MainClass extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        signUpStage = primaryStage;
+        loginStage = primaryStage;
         openLoginWindow();
         //       openWorkSpaceWindow();
     }
