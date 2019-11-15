34daac726c3424195e3bbf6b976b961bb6d4098d
adityab079 <36786132+adityab079@users.noreply.github.com>
2019-11-14 19:28:17 -0700

    Minor layout changes in Assignment pages

--- home/main/src/controllers/HomepageController.java
+++ home/main/src/controllers/HomepageController.java
@@ -5,11 +5,12 @@ package main.src.controllers;
  * Copyright:    Copyright (c) 2019
  * Company:      Department of Computer Software Engineering, Arizona State University.
  *
- * @author 		 Mehta Ria
- * @version 	 2.0
- * @since        8/30/2019
- * @modified     11/3/2019
+ * @author Mehta Ria
+ * @version 2.0
+ * @modified 11/3/2019
+ * @since 8/30/2019
  */
+
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.*;
@@ -27,7 +28,9 @@ import main.MainClass;
 
 import java.io.InputStream;
 import java.io.FileInputStream;
+
 import javafx.scene.control.Label;
+
 /**
  * The Class HomepageController.
  */
@@ -36,12 +39,14 @@ public class HomepageController {
     /** The assignment elements. */
 //    @FXML private javafx.scene.control.ComboBox submittedAssignments;
 //    @FXML private javafx.scene.control.ComboBox notSubmittedAssignments;
-    @FXML private javafx.scene.chart.BarChart assignmentComparisonChart;
-    @FXML private VBox notSubmittedVBox;
-    @FXML private VBox submittedVBox;
+    @FXML
+    private javafx.scene.chart.BarChart assignmentComparisonChart;
+    @FXML
+    private VBox notSubmittedVBox;
+    @FXML
+    private VBox submittedVBox;
     private RadioButton radioButtonAssign;
     ToggleGroup toggleGroup = new ToggleGroup();
-
 
 
     /**
@@ -49,14 +54,14 @@ public class HomepageController {
      *
      */
     @FXML
-    public void initialize(){
+    public void initialize() {
 //        String labelHead=submittedAssignments.getText()+"\n\n\n";
-        try{
+        try {
             displaySubmittedAssignments();
 //            submittedAssignments.getSelectionModel().selectFirst();
 //            notSubmittedAssignments.getSelectionModel().selectFirst();
             compareAssignmentsOnBarChart();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -77,30 +82,30 @@ public class HomepageController {
                 .build();
 
         FirebaseApp.initializeApp(options);
-        Firestore db= FirestoreClient.getFirestore();
+        Firestore db = FirestoreClient.getFirestore();
 
         /**
          * Logic for displaying list of assignments - submitted and not submitted
          */
 
         String userEmail = "karandeep@gmail.com";
-        Iterable<DocumentReference> docRefUpcoming  = db.collection("UserAssignmentStatus").document(userEmail).collection("NotSubmitted").listDocuments();
+        Iterable<DocumentReference> docRefUpcoming = db.collection("UserAssignmentStatus").document(userEmail).collection("NotSubmitted").listDocuments();
         ApiFuture<DocumentSnapshot> documentApi;
         DocumentSnapshot documentData;
 
         notSubmittedVBox.setSpacing(5);
         //Unsubmitted Assignments
-        for(DocumentReference doc :docRefUpcoming){
+        for (DocumentReference doc : docRefUpcoming) {
             documentApi = doc.get();             //Gets reference of document
-            documentData=documentApi.get();
-            radioButtonAssign =new RadioButton();
+            documentData = documentApi.get();
+            radioButtonAssign = new RadioButton();
 //            radioButtonAssign.setBorder();
             radioButtonAssign.setText(documentData.getId());
             radioButtonAssign.setToggleGroup(toggleGroup);
             notSubmittedVBox.getChildren().add(radioButtonAssign);
         }
 
-        Iterable<DocumentReference> docRefSolved  = db.collection("UserAssignmentStatus").document(userEmail).collection("Submitted").listDocuments();
+        Iterable<DocumentReference> docRefSolved = db.collection("UserAssignmentStatus").document(userEmail).collection("Submitted").listDocuments();
         ApiFuture<DocumentSnapshot> documentApiSolved;
         DocumentSnapshot documentDataSolved;
 
@@ -108,10 +113,10 @@ public class HomepageController {
         submittedVBox.setSpacing(5);
 
         //Submitted assignments
-        for(DocumentReference doc :docRefSolved){
+        for (DocumentReference doc : docRefSolved) {
             documentApiSolved = doc.get();             //Gets reference of document
-            documentDataSolved=documentApiSolved.get();
-            radioButtonAssign =new RadioButton();
+            documentDataSolved = documentApiSolved.get();
+            radioButtonAssign = new RadioButton();
             radioButtonAssign.setText(documentDataSolved.getId());
             radioButtonAssign.setToggleGroup(toggleGroup);
             submittedVBox.getChildren().addAll(radioButtonAssign);
@@ -122,13 +127,13 @@ public class HomepageController {
      * Comparing grades of assignments on bar charts
      */
 
-    void compareAssignmentsOnBarChart() throws Exception{
+    void compareAssignmentsOnBarChart() throws Exception {
 
-        Firestore db= FirestoreClient.getFirestore();
+        Firestore db = FirestoreClient.getFirestore();
 
         String userEmail = "karandeep@gmail.com";
 
-        Iterable<DocumentReference> docRefSolved  = db.collection("UserAssignmentStatus").document(userEmail).collection("Submitted").listDocuments();
+        Iterable<DocumentReference> docRefSolved = db.collection("UserAssignmentStatus").document(userEmail).collection("Submitted").listDocuments();
         ApiFuture<DocumentSnapshot> documentApiSolved;
         DocumentSnapshot documentDataSolved;
         assignmentComparisonChart.setTitle("Comparison of Assignments graded");
@@ -136,13 +141,13 @@ public class HomepageController {
         assignmentComparisonChart.getYAxis().setLabel("Grades out of 10");
         XYChart.Series<String, Number> assignments = new XYChart.Series<>();
         assignments.setName("Assignments");
-        int grade=0;
+        int grade = 0;
 
-        for(DocumentReference doc :docRefSolved){
+        for (DocumentReference doc : docRefSolved) {
             documentApiSolved = doc.get();             //Gets reference of document
-            documentDataSolved=documentApiSolved.get();
-            if(documentDataSolved.get("Grade")!=null){
-                grade=Integer.parseInt(documentDataSolved.get("Grade").toString());
+            documentDataSolved = documentApiSolved.get();
+            if (documentDataSolved.get("Grade") != null) {
+                grade = Integer.parseInt(documentDataSolved.get("Grade").toString());
             }
 
             assignments.getData().add(new XYChart.Data<>(documentDataSolved.getId(), grade));
@@ -156,16 +161,13 @@ public class HomepageController {
     }
 
 
-
-
     /**
      * Workspace action.
      *
      * @param actionEvent the action event
      */
     @FXML
-    void workspaceAction(ActionEvent actionEvent)
-    {
+    void workspaceAction(ActionEvent actionEvent) {
 
         new MainClass().openWorkSpaceWindow();
 //        MainClass.homePageStage.close();
