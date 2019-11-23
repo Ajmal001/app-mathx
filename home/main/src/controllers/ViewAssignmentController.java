ab34f48c9bd0a2bbafc932373fe29e04ab9bcfca
mmahapat <mmahapat@asu.edu>
2019-11-23 12:47:57 -0700

    Fixed a minor bug in display assignments

--- home/main/src/controllers/ViewAssignmentController.java
+++ home/main/src/controllers/ViewAssignmentController.java
@@ -71,7 +71,7 @@ public class ViewAssignmentController implements Initializable {
                 for (DataSnapshot data : dataSnapshot.getChildren()) {
                     assignmentModel2 = data.getValue(AssignmentModel.class);
                     assignmentModel2.setId(data.getKey());
-                    if(assignmentModel2.getAssignmentName().equals("Assignment1")){
+                    if(assignmentModel2.getAssignmentName().equals(Assignment)){
 
                         questionids = assignmentModel2.getQuestions();
                     }
