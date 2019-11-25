0af105a706037cd6a7f668b71e4e72a38d495848
rnmehta1 <ria.mehta@asu.edu>
2019-11-25 14:30:59 -0700

    #75 story #123 Task Pass assignment name to Workspace and optimizations
    Signed-off-by: rnmehta1 <ria.mehta@asu.edu>

--- home/main/src/controllers/HomepageController.java
+++ home/main/src/controllers/HomepageController.java
@@ -36,6 +36,7 @@ public class HomepageController {
     @FXML
     private VBox submittedVBox;
     public RadioButton radioButtonAssign;
+    public String selectedAssignment = new String("Assignment 1");
     /**
      * The assignment elements.
      */
@@ -188,9 +189,12 @@ public class HomepageController {
      */
     @FXML
     void workspaceAction(ActionEvent actionEvent) {
-//        toggleGroup.getSelectedToggle().toString().split("'")[1];
-        new MainClass().openWorkSpaceWindow();
-        MainClass.homePageStage.close();
+        selectedAssignment = toggleGroup.getSelectedToggle().toString().split("'")[1];
+        if (toggleGroup.getSelectedToggle() != null) {
+            new MainClass().openWorkSpaceWindow();
+            MainClass.homePageStage.close();
+        }
+
     }
 
     /**
